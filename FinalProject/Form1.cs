7cc74510e333f06d65f555af646aff86476fd487
jose salazar <joshrs23@gmail.com>
2023-04-15 19:56:27 -0400

    library to read Json

--- FinalProject/Form1.cs
+++ FinalProject/Form1.cs
@@ -7,14 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.Json;
 
 namespace FinalProject
 {
     public partial class Form1 : Form
     {
+        private String PathJson = @"./MOCK_DATA.json";
+        private String JsonToString;
         public Form1()
         {
             InitializeComponent();
+            JsonToString = File.
         }
     }
 }
